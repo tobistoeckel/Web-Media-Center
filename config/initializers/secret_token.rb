c4ebfbbbd2bfa9490b989cc46e9a77aca5ae96b4
Tobias Stöckel <Tobias.Stoeckel@googlemail.com>
2011-11-23 23:47:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webmc::Application.config.secret_token = '359a180dd0cd69074c8f2ae08e10f5399fa327b82311e7970e22ce2ed17beefaaa7f57e26cbd403504cc63f2c9341aa3e5138bdb714f8778a069125c8c9a6948'
