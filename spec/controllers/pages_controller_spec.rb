e124dcd07cb9f666740eadcfd9758940a59c15d3
Tobias Stöckel <Tobias.Stoeckel@googlemail.com>
2011-11-24 02:03:55 +0100

    tests for help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -30,5 +30,19 @@ describe PagesController do
     end
   
   end
+  
+  describe "GET 'help'" do
+    
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => "Web Media Center | Help")
+    end
+  
+  end
 
 end
